8ed9f9d5d6e5e46f681debd3d8d89c5c57a81a72
Falak <falakrast1@gmail.com>
2023-09-12 12:19:53 -0400

    commit

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -9,29 +9,34 @@ class FizzBuzz {
         while (u < 100) {
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = u % 3 == 0;
-            boolean divisibleBy5 = u % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(u);
-
-            }
-
-            u += 1;
+            u = doFizzBuzz(u);
         }
+    }
+
+    public static int doFizzBuzz(int u) {
+        boolean divisibleBy3 = u % 3 == 0;
+        boolean divisibleBy5 = u % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(u);
+
+        }
+
+        u += 1;
+        return u;
     }
 }
